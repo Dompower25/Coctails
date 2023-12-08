5ee6acffe8f31b70c85f6b05cbf825d79bf2135d
Eugene Domanovich <dompro1994.ed@gmail.com>
2023-12-08 22:10:01 +0300

    change preview app init components init wrapers

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,50 @@
+<script setup>
+const props = defineProps({
+  imgUrl: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="root">
+    <div :style="`background-image: url(${imgUrl})`" class="img"></div>
+    <div class="main">
+      <el-button class="main_btn">Get random cocktail</el-button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/styles/main.scss';
+@import '../assets/styles/variables.scss';
+
+.root {
+  display: flex;
+  min-height: 100vh;
+  background-color: $background;
+
+  .img {
+    width: 50%;
+    
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    background-size: cover;
+  }
+  .main {
+    position: relative;
+    width: 50%;
+    padding: 32px 40px;
+
+    .main_btn {
+        position: absolute;
+        top: 32px;
+        right: 40px;
+        border-color: $accent;
+        background-color: $accent;
+        color: $text;
+    }
+  }
+}
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,11 @@
+<script setup>
+import AppLayout from '../components/AppLayout.vue'
+</script>
+
+<template>
+  <AppLayout imgUrl="/src/assets/img/Cocktail_home_page_min.jpg" />
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/styles/main';
+</style>
